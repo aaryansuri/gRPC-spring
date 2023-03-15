13bf1e8ac623ff7666848e96118d5eda36a53383
aaryansuri <aaryansuri0@gmail.com>
2023-03-15 11:06:28 +0000

    test server

--- src/main/java/com/bank/server/GrpcServer.java
+++ src/main/java/com/bank/server/GrpcServer.java
@@ -10,7 +10,7 @@ public class GrpcServer {
     public static void main(String[] args) throws IOException, InterruptedException {
 
         Server server = ServerBuilder.forPort(6565).addService(new BankService()).build();
-
+        System.out.println("server is running");
         server.start();
         server.awaitTermination();
 
---
+++ src/test/java/com/bank/client/BankClientTest.java
@@ -0,0 +1,36 @@
+package com.bank.client;
+
+import com.bank.models.Balance;
+import com.bank.models.BalanceCheckRequest;
+import com.bank.models.BankServiceGrpc;
+import io.grpc.ManagedChannel;
+import io.grpc.ManagedChannelBuilder;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+public class BankClientTest {
+
+    private BankServiceGrpc.BankServiceBlockingStub blockingStub;
+    @BeforeAll
+    public void setup() {
+        ManagedChannel channel = ManagedChannelBuilder.forAddress("localhost", 6565)
+                .usePlaintext()
+                .build();
+        blockingStub = BankServiceGrpc.newBlockingStub(channel);
+    }
+
+    @Test
+    public void balanceTest() {
+
+        Balance balance = blockingStub.getBalance(
+                BalanceCheckRequest.newBuilder().setAccountNumber(10).build()
+        );
+
+        System.out.println(balance);
+    }
+
+
+}
